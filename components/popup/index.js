5e9931803f9a176a008405dd831d8b1086e05af6
gokce <gokcetascii@gmail.com>
2024-03-29 18:51:32 +0300

    Added why it didn't come input to be visible only when the didn't come option is selected

--- components/popup/index.js
+++ components/popup/index.js
@@ -19,24 +19,32 @@ const validate = (values) => {
 
 const Popup = ({ handleSave, handleClose }) => (
   <Formik initialValues={initialValues} validate={validate} onSubmit={handleSave}>
-    <Form>
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="relative bg-white p-4 sm:p-8 rounded-md shadow-md w-[340px] sm:w-[400px] md:w-[450px]">
-          <Field as="select" name="status">
-            <option value="Boş">Boş</option>
-            <option value="Geldi">Geldi</option>
-            <option value="Gelmedi">Gelmedi</option>
-            <option value="İzinli">İzinli</option>
-          </Field>
-          <ErrorMessage name="status" component="div" className="text-red-500" />
-          <Field type="text" name="explanation" placeholder="Neden gelmedi?" />
-          <ErrorMessage name="explanation" component="div" className="text-red-500" />
-          <button type="submit">Kaydet</button>
-          <button type="button" onClick={handleClose}>Kapat</button>
+    {({ values }) => (
+      <Form>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="relative bg-white p-4 sm:p-8 rounded-md shadow-md w-[340px] sm:w-[400px] md:w-[450px]">
+            <Field as="select" name="status">
+              <option value="Boş">Boş</option>
+              <option value="Geldi">Geldi</option>
+              <option value="Gelmedi">Gelmedi</option>
+              <option value="İzinli">İzinli</option>
+            </Field>
+            <ErrorMessage name="status" component="div" className="text-red-500" />
+            {values.status === "Gelmedi" && ( // Sadece "Gelmedi" seçeneği seçildiğinde görünsün
+              <>
+                <Field type="text" name="explanation" placeholder="Neden gelmedi?" />
+                <ErrorMessage name="explanation" component="div" className="text-red-500" />
+              </>
+            )}
+            <button type="submit">Kaydet</button>
+            <button type="button" onClick={handleClose}>Kapat</button>
+          </div>
         </div>
-      </div>
-    </Form>
+      </Form>
+    )}
   </Formik>
 );
+
+
 
 export default Popup;
