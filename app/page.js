1e84d22f92227b284785ccc635e79817949ff94c
gokce <gokcetascii@gmail.com>
2024-03-29 13:07:28 +0300

    header added

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Header from "@/components/generalcalendar/header";
 import AdminPage from "./admin/page";
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <AdminPage/>
     </div>
   );
