008c6018b7a55d450004c919802f54731d0e78ef
gokce <gokcetascii@gmail.com>
2024-04-01 14:49:01 +0300

    edited

--- app/admin/page.js
+++ app/admin/page.js
@@ -3,14 +3,32 @@ import React, { useState } from "react";
 import useStore from "@/utils/store";
 import GeneralCalendar from "@/components/generalcalendar";
 
-
 const AdminPage = () => {
   const { admin } = useStore();
-  const [allowPastAndFutureChanges, setAllowPastAndFutureChanges] = useState(false); // Geçmiş ve gelecek değişikliklere izin verme durumu
+  const [allowPastAndFutureChanges, setAllowPastAndFutureChanges] =
+    useState(false); // Geçmiş ve gelecek değişikliklere izin verme durumu
 
   return (
     <div className="container mx-auto">
       <div>
+        <div className="mt-4">
+          <label
+            htmlFor="allowChangesToggle"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Geçmiş ve gelecek değişikliklere izin ver
+          </label>
+          <div className="mt-1 flex items-center">
+            <input
+              type="checkbox"
+              id="allowChangesToggle"
+              name="allowChangesToggle"
+              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+              checked={allowPastAndFutureChanges}
+              onChange={(e) => setAllowPastAndFutureChanges(e.target.checked)}
+            />
+          </div>
+        </div>
         {/* Şubeler */}
         <div>
           <h2 className="text-xl font-semibold mb-4">Şubeler</h2>
@@ -33,28 +51,12 @@ const AdminPage = () => {
           </div>
         </div>
       </div>
-    
+
       <div className="bg-white rounded-xl p-10 shadow-2xl">
         {/* Admin Takvimi */}
         <GeneralCalendar
-          s={allowPastAndFutureChanges} // Geçmiş ve gelecek değişikliklere izin verme durumu
+          allowPastAndFutureChanges={allowPastAndFutureChanges} // Geçmiş ve gelecek değişikliklere izin verme durumu
         />
-    
-      </div>
-      <div className="mt-4">
-        <label htmlFor="allowChangesToggle" className="block text-sm font-medium text-gray-700">
-          Geçmiş ve gelecek değişikliklere izin ver
-        </label>
-        <div className="mt-1 flex items-center">
-          <input
-            type="checkbox"
-            id="allowChangesToggle"
-            name="allowChangesToggle"
-            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-            checked={allowPastAndFutureChanges}
-            onChange={(e) => setAllowPastAndFutureChanges(e.target.checked)}
-          />
-        </div>
       </div>
     </div>
   );
