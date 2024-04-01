6ef80d30fd71e9b3792d7e5ebd35bb8a274b3214
gokce <gokcetascii@gmail.com>
2024-04-01 16:51:03 +0300

    Personal calendar added instead of general calendar, edited style

--- app/employee/[id]/page.js
+++ app/employee/[id]/page.js
@@ -1,26 +1,46 @@
-"use client"
+"use client";
 import React from "react";
 import useStore from "@/utils/store";
-import GeneralCalendar from "@/components/generalcalendar";
+import PersonalCalendar from "@/components/personalcalendar"; // PersonalCalendar bileşenini import ediyoruz
+import { FcAssistant } from "react-icons/fc";
 
-function EmployeePage({ params }) { 
-    const employeeId = parseInt(params.id); 
-    const { admin } = useStore();
-  
-    // Employee bulunamazsa mesaj döndür
-    const employee = admin.branches.flatMap(branch => branch.manager.employees).find(emp => emp.id === employeeId);
-    if (!employee) return <div>Employee not found!</div>;
-  
-    // Employee'nin bağlı olduğu branch'i bul
-    const branch = admin.branches.find(branch => branch.manager.employees.some(emp => emp.id === employeeId));
-  
-    return (
-      <div>
-        <h2>{employee.name}</h2>
-        <p>Branch: {branch.name}</p>
-        <GeneralCalendar employees={[employee]} /> {/* GeneralCalendar bileşenini çağırırken ilgili işçinin bilgilerini aktarıyoruz */}
+function EmployeePage({ params }) {
+  const employeeId = parseInt(params.id);
+  const { admin } = useStore();
+
+  // Employee bulunamazsa mesaj döndür
+  const employee = admin.branches
+    .flatMap((branch) => branch.manager.employees)
+    .find((emp) => emp.id === employeeId);
+  if (!employee) return <div>Employee not found!</div>;
+
+  // Employee'nin bağlı olduğu branch'i bul
+  const branch = admin.branches.find((branch) =>
+    branch.manager.employees.some((emp) => emp.id === employeeId)
+  );
+
+  return (
+    <div className=" w-full h-screen bg-slate-50">
+      <div className="px-12 py-3 bg-gradient-to-r from-blue-400 to-indigo-200 flex flex-row items-center justify-between shadow-xl">
+        <div className="flex flex-row items-center justify-center gap-4">
+          <FcAssistant className="w-10 p-1 h-10 rounded-full border border-1 border-gray-500 bg-white" />
+          <h2 className="font-extrabold text-blue-800">{employee.name}</h2>
+        </div>
+        <div className="flex flex-row gap-7">
+          <p className="text-indigo-800">
+            <span className="text-blue-800 font-bold">Şube:</span> {branch.name}
+          </p>
+          <p className="text-indigo-800">
+            <span className="text-blue-800 font-bold">Müdür:</span>{" "}
+            {branch.manager.name}
+          </p>
+        </div>
       </div>
-    );
+
+      {/* PersonalCalendar bileşenini çağırıyoruz ve ilgili işçinin bilgilerini aktarıyoruz */}
+      <PersonalCalendar employee={employee} />
+    </div>
+  );
 }
 
 export default EmployeePage;
