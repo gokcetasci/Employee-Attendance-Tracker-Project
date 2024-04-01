1cbda4fd995de57a0c0ea87c9790291ca69246f4
gokce <gokcetascii@gmail.com>
2024-04-01 18:58:48 +0300

    edited

--- app/admin/page.js
+++ app/admin/page.js
@@ -36,28 +36,6 @@ const AdminPage = () => {
             ></div>
           </label>
         </div>
-
-        {/* Şubeler */}
-        <div>
-          <h2 className="text-xl font-semibold mb-4">Şubeler</h2>
-          <div className="flex flex-row">
-            {admin.branches.map((branch) => (
-              <div key={branch.id} className="border p-4 mb-4">
-                <h3 className="text-lg font-semibold mb-2">{branch.name}</h3>
-                <p>ID: {branch.id}</p>
-                <p>Müdür: {branch.manager.name}</p>
-
-                {/* Çalışanlar */}
-                <h4 className="text-md font-semibold mt-2">Çalışanlar</h4>
-                <ul>
-                  {branch.manager.employees.map((employee) => (
-                    <li key={employee.id}>{employee.name}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </div>
       </div>
 
       <div className="bg-white rounded-xl p-10 shadow-2xl">
--- components/header/index.js
+++ components/header/index.js
@@ -4,7 +4,9 @@ import useStore from "@/utils/store";
 import { FcBusinessman } from "react-icons/fc";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { FaStore } from "react-icons/fa6";
-import { FcManager } from "react-icons/fc";
+import { FcManager,FcAssistant } from "react-icons/fc";
+import { FcServices } from "react-icons/fc";
+
 function Header() {
   const { admin } = useStore();
   const [showDropdown, setShowDropdown] = useState(false);
@@ -25,25 +27,33 @@ function Header() {
             <MdKeyboardArrowDown className="w-6 h-6 text-gray-600 hover:text-indigo-600 hover:scale-110" />
           </span>{" "}
           {showDropdown && (
-            <ul className="absolute left-0 top-full bg-white  rounded-md mt-1 shadow-lg z-10 text-gray-500 w-[150px]" style={{ boxShadow: "0 0 4px #cbd5e1" }}>
+            <ul className="absolute left-0 top-full bg-white  rounded-md mt-1 shadow-lg z-10 text-gray-500 w-[200px]" style={{ boxShadow: "0 0 4px #cbd5e1" }}>
               {admin.branches.map((branch) => (
                 <li
                   key={branch.id}
                   className="py-1 px-3 cursor-pointer hover:bg-gray-100 "
                 >
                   <p className="flex flex-row items-center gap-2">
-                  <FaStore className="text-indigo-600"/>
+                    <FaStore className="text-indigo-600"/>
                     {branch.name}
                   </p>
                   <p className="pl-5 text-[14px] flex flex-row items-center gap-2">
-                  <FcManager />{branch.manager.name}
+                    <FcManager />{branch.manager.name}
                   </p>
+                  <ul>
+                    {branch.manager.employees.map((employee) => (
+                      <li key={employee.id} className="pl-10 text-[14px] flex flex-row items-center gap-2">
+                        <FcAssistant />{employee.name}
+                      </li>
+                    ))}
+                  </ul>
                 </li>
               ))}
             </ul>
           )}
         </div>
         <div className="flex flex-row gap-2 items-center justify-center text-gray-600 relative">
+        <FcServices className="w-7 h-10 " />
           <div className="text-right cursor-pointer">
             <h2 className="text-md font-semibold mb-1 font-medium">
               {admin.name}
