a6ca9e2be13dfbb62709eb4f92be06443cbe23dd
gokce <gokcetascii@gmail.com>
2024-04-04 11:36:44 +0300

    styles edited

--- components/header/index.js
+++ components/header/index.js
@@ -5,7 +5,6 @@ import { FcBusinessman } from "react-icons/fc";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { FaStore } from "react-icons/fa6";
 import { FcManager,FcAssistant } from "react-icons/fc";
-import { FcServices } from "react-icons/fc";
 
 function Header() {
   const { admin } = useStore();
@@ -18,13 +17,13 @@ function Header() {
   return (
     <div className="w-full h-24 p-2 bg-slate-100">
       <div
-        className="flex flex-row mx-3 my-2 bg-white rounded-md justify-between p-2"
+        className="flex flex-row mx-3 my-2 bg-gradient-to-r from-blue-200 to-indigo-200 rounded-md justify-between py-2 px-5"
         style={{ boxShadow: "0 0 4px #cbd5e1" }}
       >
         <div className="relative flex flex-row items-center justify-center ">
-          <h1 className="text-lg font-medium  text-gray-600 ">Şubeler</h1>
+          <h1 className="text-lg font-medium  text-indigo-600 ">Şubeler</h1>
           <span className="pt-1 " onClick={toggleDropdown}>
-            <MdKeyboardArrowDown className="w-6 h-6 text-gray-600 hover:text-indigo-600 hover:scale-110" />
+            <MdKeyboardArrowDown className="w-6 h-6 text-blue-600 hover:text-blue-400 hover:scale-110" />
           </span>{" "}
           {showDropdown && (
             <ul className="absolute left-0 top-full bg-white  rounded-md mt-1 shadow-lg z-10 text-gray-500 w-[200px]" style={{ boxShadow: "0 0 4px #cbd5e1" }}>
@@ -52,16 +51,15 @@ function Header() {
             </ul>
           )}
         </div>
-        <div className="flex flex-row gap-2 items-center justify-center text-gray-600 relative">
-        <FcServices className="w-7 h-10 " />
+        <div className="flex flex-row gap-2 items-center justify-center text-indigo-600 relative">
           <div className="text-right cursor-pointer">
-            <h2 className="text-md font-semibold mb-1 font-medium">
+            <h2 className="text-md font-semibold font-medium">
               {admin.name}
             </h2>
             <p className="text-sm">Admin {admin.id}</p>
           </div>
           <span>
-            <FcBusinessman className="w-10 h-10 rounded-full border border-1 border-gray-500 bg-indigo-200" />
+            <FcBusinessman className="w-10 h-10 rounded-full border border-1 border-indigo-500 bg-white" />
           </span>
         </div>
       </div>
