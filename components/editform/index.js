13ec24133bddd776df98c26d555e891124095651
gokce <gokcetascii@gmail.com>
2024-04-14 01:15:06 +0300

    edited

--- components/editform/index.js
+++ components/editform/index.js
@@ -1,70 +1,86 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";import { IoMdClose } from "react-icons/io";
 
-const EditForm = ({ handleClose }) => {
+const EditForm = ({ handleClose, handleSaveAttendance, employeeId, date }) => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    handleClose(); 
+  };
+
   return (
-      <div className="fixed inset-0 flex items-center justify-center">
-        <div className="bg-white p-8 rounded-md w-96 " style={{ boxShadow: "0 0 4px #cbd5e1" }}>
-          <h2 className="text-lg font-semibold mb-4">
+    <div className="fixed inset-0 z-50 overflow-auto bg-gray-800 bg-opacity-75 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-lg w-full max-w-md relative" style={{ boxShadow: "0 0 4px #cbd5e1" }}>
+        <div className="absolute top-2 right-2">
+          <button
+            onClick={closeModal}
+            className="text-red-500 hover:text-red-700 hover:scale-110"
+          >
+            <IoMdClose className="w-6 h-6" />
+          </button>
+        </div>
+         <h2 className="text-lg font-semibold mb-4">
             Yoklama Bilgisini Düzenle
           </h2>
-          <Formik
-            initialValues={{ status: "", explanation: "" }}
-            onSubmit={(values) => {
-              handleClose();
-            }}
-          >
-            {({ values, setFieldValue }) => (
-              <Form className="flex flex-col gap-4 mt-4">
-                <Field
-                  className="rounded-sm border border-indigo-400 text-gray-700 outline-none hover:border-indigo-600 p-1"
-                  as="select"
-                  name="status"
-                  onChange={(e) => {
-                    setFieldValue("status", e.target.value);
-                    if (e.target.value === "Gelmedi") {
-                      setFieldValue("explanationVisible", true);
-                    } else {
-                      setFieldValue("explanationVisible", false);
-                    }
-                  }}
+        <Formik
+          initialValues={{ status: "", explanation: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            // Formu backend'e gönder
+            handleSaveAttendance(employeeId, date, values);
+            setSubmitting(false);
+            closeModal(); // Form gönderildikten sonra modalı kapat
+          }}
+        >
+          {({ values, setFieldValue }) => (
+            <Form className="flex flex-col gap-2 text-sm">
+              <div className="flex flex-row gap-2 text-sm">
+              <Field
+                className="w-64 rounded-sm border border-indigo-400 text-gray-700 outline-none hover:border-indigo-600 p-1"
+                as="select"
+                name="status"
+                onChange={(e) => {
+                  setFieldValue("status", e.target.value);
+                  if (e.target.value === "Gelmedi") {
+                    setFieldValue("explanationVisible", true);
+                  } else {
+                    setFieldValue("explanationVisible", false);
+                  }
+                }}
+              >
+                <option value="">Boş</option>
+                <option value="Geldi">Geldi</option>
+                <option value="Gelmedi">Gelmedi</option>
+                <option value="İzinli">İzinli</option>
+              </Field>
+              
+              <div>
+                <button
+                  id="checkbutton"
+                  type="submit"
+                  className="hover:scale-105 bg-blue-600 px-4 py-2 rounded-md text-white ml-6"
                 >
-                  <option value="">Durum Seçin</option>
-                  <option value="Geldi">Geldi</option>
-                  <option value="Gelmedi">Gelmedi</option>
-                  <option value="İzinli">İzinli</option>
-                </Field>
-
-                {values.explanationVisible && (
-                  <div className="relative">
-                    <Field
-                      className="flex gap-4 border-2 border-blue-300 rounded-md px-2 py-1 hover:border-indigo-400 outline-none"
-                      type="text"
-                      name="explanation"
-                      placeholder="Neden Gelmedi?"
-                    />
-                  </div>
-                )}
-
-                <div className="flex flex-row gap-3 justify-end">
-                  <button
-                    className="px-4 mt-4 py-2 bg-blue-500 text-white rounded-full"
-                    type="submit"
-                  >
-                    Kaydet
-                  </button>
-                  <button
-                    className="bg-red-500 text-white px-4 py-2 rounded-full mt-4 font-medium hover:scale-105"
-                    onClick={handleClose}
-                  >
-                    Kapat
-                  </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
+                  Değiştir
+                </button>
+              </div>
+              </div>
+              <div>
+              {values.explanationVisible && (
+                <Field
+                className="flex gap-4 border-2 border-blue-300 rounded-md px-2 py-1 hover:border-indigo-400 outline-none w-full"
+                  type="text"
+                  name="explanation"
+                  placeholder="Neden Gelmedi?"
+                />
+              )}
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
+    </div>
   );
 };
 
